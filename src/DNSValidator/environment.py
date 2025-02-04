40661f7eed2d6df7d655300b2c39ce48e4def0bb
Nathan <nmalcolm@users.noreply.github.com>
2025-02-04 20:50:02 +0400

    Update environment.py

--- src/DNSValidator/environment.py
+++ src/DNSValidator/environment.py
@@ -7,14 +7,15 @@ regex = re.compile(r'\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[.]){3}(?:25[0
 baselinesrvs = [
     '1.1.1.1', #-> OpenDNS
     '8.8.8.8', #-> Google
+    '8.8.4.4', #-> Google
     '9.9.9.9', #-> Quad9
 ]
 
 
 ##->> Baseline domains to validate against
 baselinechecks = [
-    'bet365.com',
-    'telegram.com'
+    'starbucks.com.sg',
+    'mayoclinic.org',
 ]
 
 ##->> Domains to check form DNS Poisoning
